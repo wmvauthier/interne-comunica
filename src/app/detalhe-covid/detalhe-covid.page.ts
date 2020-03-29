516238218c1b741d6dda803e6ae4b79f6dedcb53
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-03-29 06:06:02 -0300

    A

--- src/app/detalhe-covid/detalhe-covid.page.ts
+++ src/app/detalhe-covid/detalhe-covid.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-covid',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalheCovidPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  go(page) {
+    switch (page) {
+      case 0:
+        this.router.navigateByUrl('/home');
+        break;
+      case 1:
+        this.router.navigateByUrl('/video');
+        break;
+      case 2:
+        this.router.navigateByUrl('/comunicado');
+        break;
+      case 3:
+        this.router.navigateByUrl('/covid');
+        break;
+      default:
+        break;
+    }
+  }
 }
